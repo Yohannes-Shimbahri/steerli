a613516e88ec1e3a28266a95196519168dced51d
yohannes kidane <jhovanisabuchi@gmail.com>
2026-01-30 16:38:31 -0500

    updated the smtp_port

--- api/contact.js
+++ api/contact.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_PORT == 587,
+    secure: process.env.SMTP_PORT == 465,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
